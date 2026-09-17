42e3a4324c82a63376af71eae491202e7244a697
Vinit <vinitrajak8235@gmail.com>
2026-09-17 10:32:29 +0530

    Time: 5 ms (82.86%), Space: 77.2 MB (85.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,29 +1,30 @@
-class Solution 
-{
-    public int maxArea(int[] height) 
-    {
-    int left=0;//start from 1st line
-    int right=height.length-1;//start from lst line
-    int maxWater=0;//it will store max water found
+// class Solution 
+// {
+//     public int maxArea(int[] height) 
+//     {
+//     int left=0;//start from 1st line
+//     int right=height.length-1;//start from lst line
+//     int maxWater=0;//it will store max water found
 
-    while(left<right)//keep checking of left right not meet
-    {
-        int width=right-left;//dis between 2 lines
-        int h=Math.min(height[left],height[right]);//water height  is decided by shorter line
-        int area=width*h;//calc water 
-        maxWater=Math.max(maxWater,area);//save bigger value between old and curr max water
+//     while(left<right)//keep checking of left right not meet
+//     {
+//         int width=right-left;//dis between 2 lines
+//         int h=Math.min(height[left],height[right]);//water height  is decided by shorter line
+//         int area=width*h;//calc water 
+//         maxWater=Math.max(maxWater,area);//save bigger value between old and curr max water
 
-        //move left one step if left line is small
-        if(height[left]<height[right]){
-            left++;
-        }
-        else{
-            right--;
-        }
-    }  
-    return maxWater;
-    }
-}
+//         //move left one step if left line is small
+//         if(height[left]<height[right]){
+//             left++;
+//         }
+//         else{
+//             right--;
+//         }
+//     }  
+//     return maxWater;
+//     }
+// }
+
 // /* To solve this efficiently in O(n) time, use the Two Pointer approach.
 // Start with two pointers:
 // left = 0; right = n - 1
@@ -32,18 +33,20 @@ class Solution
 // height = min(height[left], height[right])
 // Move the pointer with the smaller height, because moving the taller one cannot increase area. */ 
 
-// public int maxArea(int[] height) {
-//     int l = 0, r = height.length - 1;
-//     int maxArea = 0;
-//     while (l < r) {
-//         int area = Math.min(height[l], height[r]) * (r - l);
-//         maxArea = Math.max(maxArea, area);
-//         if (height[l] < height[r]) {
-//             l++;
-//         } else {
-//             r--;
-//         }
-//     }
-//     return maxArea;
-// }
+class Solution{
+public int maxArea(int[] height) {
+    int l = 0, r = height.length - 1;
+    int maxArea = 0;
+    while (l < r) {
+        int area = Math.min(height[l], height[r]) * (r - l);
+        maxArea = Math.max(maxArea, area);
+        if (height[l] < height[r]) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+    return maxArea;
+}
+}
 
